1b500555a1c6d7120849fe9919bbf6622864a46e
堀内 大輔 <horiuchi.daisuke@moneyforward.co.jp>
2025-05-28 19:15:01 +0900

    TravelPlannerUI コンポーネント追加

---
+++ app/TravelPlannerUI.tsx
@@ -0,0 +1,8 @@
+export default function TravelPlannerUI() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Budget Travel Planner</h1>
+      <p>ようこそ！旅行の条件を入力して、最適なプランをご提案します。</p>
+    </div>
+  );
+}
